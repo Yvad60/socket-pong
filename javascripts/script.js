f882b65475e5f039ce17f174f9e90f5dc5365e15
Ivad <yvesyvad@gmail.com>
2023-10-10 17:26:28 +0200

    feat: add socket implementation

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,7 +1,13 @@
-// Canvas Related 
-const canvas = document.createElement('canvas');
-const context = canvas.getContext('2d');
+// Canvas Related
+const canvas = document.createElement("canvas");
+const context = canvas.getContext("2d");
+let isReferee = false;
 let paddleIndex = 0;
+
+const socket = io("http://localhost:5000");
+socket.on("connect", () => {
+  console.log("we are now connected as", socket.id);
+});
 
 let width = 500;
 let height = 700;
@@ -10,8 +16,8 @@ let height = 700;
 let paddleHeight = 10;
 let paddleWidth = 50;
 let paddleDiff = 25;
-let paddleX = [ 225, 225 ];
-let trajectoryX = [ 0, 0 ];
+let paddleX = [225, 225];
+let trajectoryX = [0, 0];
 let playerMoved = false;
 
 // Ball
@@ -26,11 +32,11 @@ let speedX = 0;
 let computerSpeed = 4;
 
 // Score for Both Players
-let score = [ 0, 0 ];
+let score = [0, 0];
 
 // Create Canvas Element
 function createCanvas() {
-  canvas.id = 'canvas';
+  canvas.id = "canvas";
   canvas.width = width;
   canvas.height = height;
   document.body.appendChild(canvas);
@@ -38,25 +44,25 @@ function createCanvas() {
 }
 
 // Wait for Opponents
-// function renderIntro() {
-//   // Canvas Background
-//   context.fillStyle = 'black';
-//   context.fillRect(0, 0, width, height);
+function renderIntro() {
+  // Canvas Background
+  context.fillStyle = "black";
+  context.fillRect(0, 0, width, height);
 
-//   // Intro Text
-//   context.fillStyle = 'white';
-//   context.font = "32px Courier New";
-//   context.fillText("Waiting for opponent...", 20, (canvas.height / 2) - 30);
-// }
+  // Intro Text
+  context.fillStyle = "white";
+  context.font = "32px Courier New";
+  context.fillText("Waiting for opponent...", 20, canvas.height / 2 - 30);
+}
 
 // Render Everything on Canvas
 function renderCanvas() {
   // Canvas Background
-  context.fillStyle = 'black';
+  context.fillStyle = "black";
   context.fillRect(0, 0, width, height);
 
   // Paddle Color
-  context.fillStyle = 'white';
+  context.fillStyle = "white";
 
   // Bottom Paddle
   context.fillRect(paddleX[0], height - 20, paddleWidth, paddleHeight);
@@ -69,19 +75,19 @@ function renderCanvas() {
   context.setLineDash([4]);
   context.moveTo(0, 350);
   context.lineTo(500, 350);
-  context.strokeStyle = 'grey';
+  context.strokeStyle = "grey";
   context.stroke();
 
   // Ball
   context.beginPath();
   context.arc(ballX, ballY, ballRadius, 2 * Math.PI, false);
-  context.fillStyle = 'white';
+  context.fillStyle = "white";
   context.fill();
 
   // Score
   context.font = "32px Courier New";
-  context.fillText(score[0], 20, (canvas.height / 2) + 50);
-  context.fillText(score[1], 20, (canvas.height / 2) - 30);
+  context.fillText(score[0], 20, canvas.height / 2 + 50);
+  context.fillText(score[1], 20, canvas.height / 2 - 30);
 }
 
 // Reset Ball to Center
@@ -89,16 +95,19 @@ function ballReset() {
   ballX = width / 2;
   ballY = height / 2;
   speedY = 3;
+  socket.emit("ballMove", { ballX, ballY, score });
 }
 
 // Adjust Ball Movement
 function ballMove() {
+  console.log("the ball is moving");
   // Vertical Speed
   ballY += speedY * ballDirection;
   // Horizontal Speed
   if (playerMoved) {
     ballX += speedX;
   }
+  socket.emit("ballMove", { ballX, ballY, score });
 }
 
 // Determine What Ball Bounces Off, Score Points, Reset Ball
@@ -154,54 +163,76 @@ function ballBoundaries() {
       score[0]++;
     }
   }
+  socket.emit("ballMove", { ballX, ballY, score });
 }
 
 // Computer Movement
-function computerAI() {
-  if (playerMoved) {
-    if (paddleX[1] + paddleDiff < ballX) {
-      paddleX[1] += computerSpeed;
-    } else {
-      paddleX[1] -= computerSpeed;
-    }
-    if (paddleX[1] < 0) {
-      paddleX[1] = 0;
-    } else if (paddleX[1] > (width - paddleWidth)) {
-      paddleX[1] = width - paddleWidth;
-    }
-  }
-}
+// function computerAI() {
+//   if (playerMoved) {
+//     if (paddleX[1] + paddleDiff < ballX) {
+//       paddleX[1] += computerSpeed;
+//     } else {
+//       paddleX[1] -= computerSpeed;
+//     }
+//     if (paddleX[1] < 0) {
+//       paddleX[1] = 0;
+//     } else if (paddleX[1] > width - paddleWidth) {
+//       paddleX[1] = width - paddleWidth;
+//     }
+//   }
+// }
 
 // Called Every Frame
 function animate() {
-  computerAI();
-  ballMove();
+  if (isReferee) {
+    ballMove();
+    ballBoundaries();
+  }
   renderCanvas();
-  ballBoundaries();
   window.requestAnimationFrame(animate);
 }
 
 // Start Game, Reset Everything
-function startGame() {
+function loadGame() {
   createCanvas();
-  // renderIntro();
-  
-  paddleIndex = 0;
+  renderIntro();
+  socket.emit("ready");
+}
+
+function startGame() {
+  paddleIndex = isReferee ? 0 : 1;
   window.requestAnimationFrame(animate);
-  canvas.addEventListener('mousemove', (e) => {
+  canvas.addEventListener("mousemove", (e) => {
     playerMoved = true;
     paddleX[paddleIndex] = e.offsetX;
     if (paddleX[paddleIndex] < 0) {
       paddleX[paddleIndex] = 0;
     }
-    if (paddleX[paddleIndex] > (width - paddleWidth)) {
+    if (paddleX[paddleIndex] > width - paddleWidth) {
       paddleX[paddleIndex] = width - paddleWidth;
     }
+
+    socket.emit("paddleMove", { xPosition: paddleX[paddleIndex] });
     // Hide Cursor
-    canvas.style.cursor = 'none';
+    canvas.style.cursor = "none";
   });
 }
 
 // On Load
-startGame();
+loadGame();
 
+socket.on("gameStart", (refereeId) => {
+  if (refereeId === socket.id) isReferee = true;
+  startGame();
+});
+
+socket.on("paddleMove", (paddleData) => {
+  const opponentPaddleIndex = 1 - paddleIndex;
+  paddleX[opponentPaddleIndex] = paddleData.xPosition;
+});
+
+socket.on("ballMove", (ballData) => {
+  ballY = ballData.ballY;
+  ballX = ballData.ballX;
+  score = ballData.score;
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,8 +4,34 @@ import { Server } from "socket.io";
 const PORT = 5000;
 
 const server = http.createServer();
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
 server.listen(PORT, () => {
   console.log("Server listenning on port 5000...");
 });
 
-const io = new Server(server);
+let readyPlayers = 0;
+
+io.on("connection", (socket) => {
+  socket.on("ready", () => {
+    console.log(`Player is ready ${socket.id}`);
+    readyPlayers++;
+    if (readyPlayers === 2) {
+      console.log("2 players are ready");
+      io.emit("gameStart", socket.id);
+    }
+  });
+
+  socket.on("paddleMove", (paddleData: { xPosition: number }) => {
+    socket.broadcast.emit("paddleMove", paddleData);
+  });
+
+  socket.on("ballMove", (ballData) => {
+    socket.broadcast.emit("ballMove", ballData);
+  });
+});
